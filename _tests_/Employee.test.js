bc0d2f2d69470f1a40906ba06ec3bf3a6fe4bf11
Adem Sahil <adem.sahil274@gmail.com>
2023-03-05 12:18:33 -0500

    space in Employee instance, const e = newEmployee();

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,7 +1,7 @@
 const Employee = require('../lib/Employee');
 
 test("Can instantiate Employee instance", () => {
-    const e = newEmployee();
+    const e = new Employee();
     expect(typeof(e)).toBe("object");
 });
 
